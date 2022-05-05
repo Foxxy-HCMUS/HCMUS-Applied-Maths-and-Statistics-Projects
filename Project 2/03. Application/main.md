804cf12ac51287127aac0770d145c81666f20b48
Mekanican <boommerang73@gmail.com>
2022-05-05 17:56:50 +0700

    change dataset type & analyze on sentences

--- Project 2/03. Application/main.md
+++ Project 2/03. Application/main.md
@@ -72,7 +72,11 @@ Dataset lưu ở `./treebank/treebank/tagged`, copy toàn bộ file `*.pos` tớ
 cp ~/archive/treebank/treebank/tagged/*.pos ./dataset/
 ```
 
-Theo `treebank/treebank/tagged/README`, dữ liệu ban đầu đã được chạy qua PARTS (Ken Church's stochastic part-of-speech tagger), sau đó được sửa lại thông qua người gán nhãn. Một số điểm cần phải sửa đổi sau khi load dataset (Các file `.pos`):
-- Dấu ngoặc vuông ( **\[ \]** ) được tạo với PARTS là dư thừa, bỏ qua trong quá trình xử lí
+Theo `treebank/treebank/combined/README`, dữ liệu ban đầu đã được chạy qua PARTS (Ken Church's stochastic part-of-speech tagger), sau đó được sửa lại thông qua người gán nhãn, ghép với câu dữ liệu gốc tạo thành file Bracket. Một số điểm cần phải sửa đổi sau khi load dataset (Các file `.mrg`):
 - Các kí hiệu có nhãn dán riêng biệt, để đơn giản cho việc xử lí và thống nhất với các nhãn dán đã liệt kê ở trên, thay đổi nhãn của ký hiệu thành `SYM`
-- Một số từ không chắc chắn thì sẽ có nhiều hơn 1 nhãn. Tuy nhiên, `TaggedCorpusReader` từ thư viện `nltk.corpus.reader` chỉ có thể đọc 1 nhãn (cuối cùng). Ví dụ `"A/NNP/FW"` sẽ được đọc thành `("A/NNP", "FW")`, ta cần xử lý phần word (loại bỏ tag dư) để trở thành `("A", "FW")`
+- Một số từ không có nhãn, trong dataset được gán là `-NONE-`, cần lọc ra những từ này trước khi xử lí.
+# 3.3 Mô tả các thành phần của mô hình
+- Tập các trạng thái ẩn: là tập các tag có thể của mỗi từ (dựa trên quy tắc gán nhãn của Penn TreeBank dataset).
+
+> S = {"CC", "CD", "DT", "EX", ..., "WRB"}
+- Các quan sát có thể
--- Project 2/03. Application/main.py
+++ Project 2/03. Application/main.py
@@ -1,46 +1,95 @@
-# from prettytable import PrettyTable
-# t = PrettyTable(['Name', 'Age'])
-# t.add_row(['Alice', 24])
-# t.add_row(['Bob', 19])
-# print(t)
-
-from nltk.corpus.reader import TaggedCorpusReader
+from nltk.corpus.reader import BracketParseCorpusReader
 import glob
-from prettytable import PrettyTable
 import string
 
 
 dataset_path = './dataset'
-list_of_file = glob.glob(dataset_path + '/*.pos') # Lấy đường dẫn toàn bộ file trong thư mục
+list_of_file = glob.glob(dataset_path + '/train/' + '/*.mrg') # Lấy đường dẫn toàn bộ file trong thư mục
 
-reader_corpus = TaggedCorpusReader('.', list_of_file)
+reader_corpus = BracketParseCorpusReader('.', list_of_file)
 
-list_of_tagged = reader_corpus.tagged_words() # Đọc dữ liệu
-# Kết quả sau khi đọc (15 kết quả đầu tiên)
+# Đọc dữ liệu các câu
+list_of_tagged_sents = reader_corpus.tagged_sents() 
 
-def printList(tList, size = 15):
-    t = PrettyTable(['Word', 'Tag'])
-    for i in tList[:size]:
-        t.add_row([i[0], i[1]])
-    print(t)
+def printSentence(tag_sent, word_per_line = 2):
+    word_per_line = min(word_per_line, len(tag_sent) - 1)
+    print('[', end='')
+    for i in range(0, len(tag_sent)):
+        if i % word_per_line == 0:
+            print('')
+            print('\t', end = '')
+        print(tag_sent[i], end = ', ')
+    print('\n]')
 
-printList(list_of_tagged)
 
-# Lọc bỏ những từ không có tag (None)
-list_of_tagged = list(filter(
-    lambda x: x[1] != None, 
-    list_of_tagged
+print("Du lieu ban dau:")
+printSentence(list_of_tagged_sents[0])
+
+# Lọc bỏ những từ không có tag (-NONE-)
+list_of_tagged_sents = list(map(
+    lambda sent: list(filter(
+        lambda word: word[1] != '-NONE-',
+        sent
+    )),
+    list_of_tagged_sents
 ))
+
 # Chuyển đổi tag dấu câu thành SYM
-list_of_tagged = list(map(
-    lambda x: (x[0], "SYM") if (x[0][0] in string.punctuation) else x, 
-    list_of_tagged
+list_of_tagged_sents = list(map(
+    lambda sent: list(map(
+        lambda word: (word[0], "SYM") if word[1][0] in string.punctuation else word,
+        sent
+    )),
+    list_of_tagged_sents
 ))
-# Lọc tag thừa
-list_of_tagged = list(map(
-    lambda x: (x[0][:x[0].find("/")], x[1]) if x[0].find("/") != -1 else x,
-    list_of_tagged
-))
-
 print("Ket Qua sau khi loc: ")
-printList(list_of_tagged)
+printSentence(list_of_tagged_sents[0])
+
+# Vd bigram: [1,2,3,4] -> [(1,2), (2,3), (3,4)]
+# P(t_i | t_{i - 1})
+def bigramCount(tag_sent_list):
+    # Tạo bigram trên tag
+    # Bigram được tạo riêng trên mỗi câu!
+
+    bigram = [
+        (sent[i][1], sent[i + 1][1]) for sent in tag_sent_list for i in range(len(sent) - 1)
+    ]
+
+    map_count = {}
+    for x in bigram:
+        if x in map_count:
+            map_count[x] += 1
+        else:
+            map_count[x] = 1
+    return map_count
+
+# Tương tự với unigram
+def unigramCount(tag_sent_list):
+    unigram = [
+        word[1] for sent in tag_sent_list for word in sent
+    ]
+    map_count = {}
+    for x in unigram:
+        if x in map_count:
+            map_count[x] += 1
+        else:
+            map_count[x] = 1
+    return map_count
+
+# Tagset (36 loại)
+tag_set = [
+    'JJS', 'PRP$', 'WDT', 'NNP', 'TO', 'PDT', 'WRB', 'WP', 'NNS', 'VB', 'MD', 'RP', 
+    'PRP', 'JJR', 'JJ', 'VBZ', 'RBS', 'VBG', 'POS', 'VBD', 'NN', 'UH', 'FW', 'NNPS', 
+    'WP$', 'EX', 'SYM', 'RBR', 'VBN', 'LS', 'IN', 'DT', 'VBP', 'CD', 'RB', 'CC'
+]
+
+bi_cnt = bigramCount(list_of_tagged_sents)
+uni_cnt = unigramCount(list_of_tagged_sents)
+
+print("5 bigram có số lần xuất hiện lớn nhất: ")
+for i in sorted(bi_cnt, key = bi_cnt.get)[-5:]:
+    print((i, bi_cnt[i]))
+
+print("5 unigram  có số lần xuất hiện lớn nhất: ")
+for i in sorted(uni_cnt, key = uni_cnt.get)[-5:]:
+    print((i, uni_cnt[i]))
